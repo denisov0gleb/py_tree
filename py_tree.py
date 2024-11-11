984ceac65d5744af799bfef697531d996d570299
Denisov Gleb <denisov0gleb@gmail.com>
2024-11-11 23:39:27 +0300

    Added main fuction and checking for main execution

--- py_tree.py
+++ py_tree.py
@@ -1,15 +1,21 @@
 import os
 
 
-initial_path = os.path.abspath('.')
-dirs_list= initial_path.split('\\')
+def main() -> None:
+    initial_path = os.path.abspath('.')
+    dirs_list= initial_path.split('\\')
 
-path_string = ''
-for index, dir_name in enumerate(dirs_list):
-    if index == 0:
-        path_string += dir_name
-    else:
-        path_string += '\\' + dir_name
+    path_string = ''
+    for index, dir_name in enumerate(dirs_list):
+        if index == 0:
+            path_string += dir_name
+        else:
+            path_string += '\\' + dir_name
 
-        print(*os.listdir(path_string), end='\n\n')
-print(dirs_list)
+            print(*os.listdir(path_string), end='\n\n')
+    print(dirs_list)
+
+
+
+if __name__ == "__main__":
+    main()

06151a6ee8f8817e4f06098bd2dcad46b9a20ec7
Efim <Efimfedoseev76@gmail.com>
2024-11-16 18:09:30 +0300

    enclosed all functions in a class

--- py_tree.py
+++ py_tree.py
@@ -1,32 +1,40 @@
 import os
 
 
-def defining_folders():
-    initial_path = os.path.abspath('.')
-    dirs_list = initial_path.split('\\')
-    return_list = os.listdir(initial_path)
+class TestClass:
 
-    return return_list
-    # print(dirs_list) #Test_prints
-    # print(initial_path)
-    # print(return_list)
+    def __init__(self, initial_path):
+        self.initial_path = initial_path
+
+    def defining_folders(self):
+        # self.initial_path = os.path.abspath('.')
+        # self.dirs_list = self.initial_path.split('\\')
+        return_list = os.listdir(self.initial_path)
+
+        return return_list
+
+        # print(self.initial_path) #Test_prints
+        # print(self.return_list)
+
+    def check_directory(self):
+        test_dict = {}
+        content = self.defining_folders()
+        # print(content)
+
+        for i in content:
+            if os.path.isfile(i):
+                test_dict[i] = 'It is file'
+
+            if os.path.isdir(i):
+                test_dict[i] = 'It is directory'
+
+        return test_dict
+
+    def main(self):
+        if __name__ == "__main__":
+            final_result = self.check_directory()
+            print(final_result)
 
 
-def check_directory():
-    test_dict = {}
-    content = defining_folders()
-    # print(content)
-
-    for i in content:
-        if os.path.isfile(i):
-            test_dict[i] = 'It is file'
-
-        if os.path.isdir(i):
-            test_dict[i] = 'It is directory'
-
-    return test_dict
-
-
-if __name__ == "__main__":
-    final_result = check_directory()
-    print(final_result)
+test = TestClass(initial_path=os.path.abspath('.'))
+test.main()

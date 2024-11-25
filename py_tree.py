a8f6b9d604e2696a65e01ea97f4413de818bf9d9
EReid <egornen807@gmail.com>
2024-11-26 02:14:09 +0300

    defs: git_files_list and git_dirs_list

--- py_tree.py
+++ py_tree.py
@@ -6,11 +6,13 @@ class DirectoryContent:
     def __init__(self, initial_path):
         self.initial_path = initial_path
 
+        self.return_list = os.listdir(self.initial_path)
+        print(self.return_list)
+
     def check_directory(self):
         test_dict = {}
-        return_list = os.listdir(self.initial_path)
 
-        for i in return_list:
+        for i in self.return_list:
             if os.path.isfile(i):
                 test_dict[i] = 'It is file'
 
@@ -19,9 +21,31 @@ class DirectoryContent:
 
         return test_dict
 
+    def get_files_liest(self):
+        files_list = []
+
+        for i in self.return_list:
+            if os.path.isfile(i):
+                files_list.append(i)
+
+        return files_list
+
+    def get_dirs_list(self):
+        ders_list = []
+
+        for i in self.return_list:
+            if os.path.isdir(i):
+                ders_list.append(i)
+
+        return ders_list
+
     def main(self):
         final_result = self.check_directory()
         print(final_result)
+        print(self.get_files_liest())
+        print(self.get_dirs_list())
+
+
 
 
 if __name__ == "__main__":
